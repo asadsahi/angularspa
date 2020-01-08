5931bbc4600dd73a1cf3f4a4f29012651bd737cc
Asad Sahi <asadazmat@gmail.com>
2020-01-08 11:28:35 +0000

    fix di error

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -11,13 +11,13 @@ import { LoadingService } from '../../services';
 export class LoadingComponent implements AfterViewInit, OnDestroy {
   loadingSubscription: Subscription;
 
-  constructor(private loadingScreenService: LoadingService, private _elmRef: ElementRef, private _changeDetectorRef: ChangeDetectorRef) {}
+  constructor(private loadingScreenService: LoadingService, private elmRef: ElementRef, private changeDetectorRef: ChangeDetectorRef) {}
 
   ngAfterViewInit(): void {
-    this._elmRef.nativeElement.style.display = 'none';
+    this.elmRef.nativeElement.style.display = 'none';
     this.loadingSubscription = this.loadingScreenService.loading$.pipe().subscribe((status: boolean) => {
-      this._elmRef.nativeElement.style.display = status ? 'block' : 'none';
-      this._changeDetectorRef.detectChanges();
+      this.elmRef.nativeElement.style.display = status ? 'block' : 'none';
+      this.changeDetectorRef.detectChanges();
     });
   }
 
